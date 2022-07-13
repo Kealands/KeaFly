16ff67f01422b4a9d1b10499d19966ec4b35704f
Beary <36056927+itsbeary@users.noreply.github.com>
2022-07-13 21:02:08 +1200

    remove all files

--- src/main/java/net/beary/keafly/KeaFly.java
+++ src/main/java/net/beary/keafly/KeaFly.java
@@ -1,24 +1,16 @@
 package net.beary.keafly;
 
-import net.beary.keafly.commands.KeaFlyCommand;
-import net.beary.keafly.managers.FlyManager;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class KeaFly extends JavaPlugin {
 
-    private static KeaFly instance;
-    public static KeaFly getInstance() { return instance; }
-
+    private static KeaFly inst;
+    public static KeaFly inst() { return inst; }
 
     @Override
     public void onEnable() {
+        inst = this;
         saveDefaultConfig();
-
-        Bukkit.getServer().getPluginManager().registerEvents(new FlyManager(), this);
-        getCommand("keafly").setExecutor(new KeaFlyCommand());
-
-
     }
 }
